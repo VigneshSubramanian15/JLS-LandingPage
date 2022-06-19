181eb63a51fe1478637944ce89ddb55eebfcb79e
vigneshsubramanian7 <vigneshs@dckap.com>
2022-06-19 18:46:46 +0530

    BG Change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "./../styles/style.sass";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
